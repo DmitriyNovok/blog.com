01d859a5bc8ca8d23d82b6790eaf2b2359e04b6d
DmitriyNovok <steamap@mail.ru>
2019-11-05 00:09:03 +0300

    add controllers

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$router->get('/', 'ViewController@viewMain');
 
-$router->get('/', function () use ($router) {
-    return view('index');
-});
+$router->get('/about', 'ViewController@viewAbout');
 
-$router->get('/about', function () use ($router) {
-    return view('inside');
-});
+$router->get('/services', 'ViewController@viewServices');
 
-$router->get('/services', function () use ($router) {
-    return view('services');
-});
-
-$router->get('/contact', function () use ($router) {
-    return view('contact');
-});
+$router->get('/contact', 'ViewController@viewContacts');
